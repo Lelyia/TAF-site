aa2b4f5f6b97ba804b472ccead7089fcd5b8b5c1
Olga <olga@bukhina.ru>
2024-10-26 01:43:00 +0300

    +1

--- about/about.js
+++ about/about.js
@@ -5,3 +5,16 @@ const navbar = document.querySelector('.header');
 menuToggle.addEventListener('click', () => {
     navbar.classList.toggle('active');
 });
+
+// Функция изменения фона при скролле
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      header.style.backgroundColor = '#001d3d'; // Тёмный фон
+    } else {
+      header.style.backgroundColor = 'rgba(0, 0, 0, 0.2)'; // Прозрачный фон
+    }
+  });
+});
